598548a946888a5409649c2fa60b4a565fc3a159
watchbutdonotlearn <71526591+watchbutdonotlearn@users.noreply.github.com>
2021-03-25 13:07:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function myFunction() {
   alert("LOL YOU'VE BEEN RICKROLLED LOL");
 }
+
+function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+        return window.clipboardData.setData("Text", text);
+
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        }
+        finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
